2ef32cc37318b60e035e413a95435b805caadcba
petro-mykhats2 <02webproject02@gmail.com>
2022-11-20 18:18:22 +0100

    add graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: `SiteName - main idea or description`,
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+    'gatsby-transformer-json',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sass',
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,38 @@
+import { graphql } from 'gatsby'
 import React from 'react'
+import Item from '../components/Item'
 import Layout from '../layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1 className='container'>Main content</h1>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const orders = data.allProductsJson.edges
+  return (
+    <Layout>
+      <h1 className='container'>Main content</h1>
+      <h1>
+        {orders.map((order) => {
+          return <Item key={order.node.id} orderdata={order.node} />
+        })}
+      </h1>
+      {/* <---  JSON preview data  ---> */}
+      {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allProductsJson {
+      edges {
+        node {
+          title
+          price
+          parameters
+          slug
+          id
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
