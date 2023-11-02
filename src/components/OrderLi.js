c739ba753d339995ef862aa74ab640a834416b64
petro-mykhats2 <02webproject02@gmail.com>
2023-11-02 21:47:39 +0100

    Expanded the page for {product type name} and displayed all products of this type.

--- src/components/OrderLi.js
+++ src/components/OrderLi.js
@@ -3,10 +3,14 @@ import OrderBlock from './OrderBlock'
 import OrderLiTop from './OrderLiTop'
 
 function OrderLi({ block, products, customkey, name }) {
-  // console.log('datas in orderLi', block, products)
   return (
     <div className='order_li'>
-      <OrderLiTop customKey={block.id} block={block} name={name} />
+      <OrderLiTop
+        customKey={block.id}
+        block={block}
+        name={name}
+        products={products}
+      />
       <OrderBlock customKey={customkey} products={products} />
     </div>
   )
--- src/pages/menu/{MarkdownRemark.fields__slug}.js
+++ src/pages/menu/{MarkdownRemark.fields__slug}.js
@@ -4,20 +4,21 @@ import Item from '../../components/Item'
 import Layout from '../../layout'
 
 function All({ data }) {
-  console.log('datammmm', data)
-  // const orders = data.allMarkdownRemark.edges
   const title = data.markdownRemark.frontmatter.title
+  const name = data.markdownRemark.frontmatter.name
+
+  // Фільтрація продуктів за умовою type === title
+  const filteredProducts = data.allProducts.edges.filter(
+    ({ node }) => node.frontmatter.type === title
+  )
+
   return (
     <Layout>
-      {title} jhhjhjhjhjh - новий
-      <div className='rolls'>
-        {/* {orders.map((order) => {
-          return (
-            <div className='rollItem'>
-              <Item key={order.node.id} orderdata={order.node} />
-            </div>
-          )
-        })} */}
+      <h1>{name}</h1>
+      <div className='rollsss'>
+        {filteredProducts.map(({ node }) => (
+          <Item key={node.id} orderdata={node} />
+        ))}
       </div>
     </Layout>
   )
@@ -33,6 +34,32 @@ export const query = graphql`
       }
       frontmatter {
         title
+        name
+      }
+    }
+    allProducts: allMarkdownRemark {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            templateKey
+            sku
+            weight
+            title
+            price
+            image
+            category
+            parameters
+            mainCategory
+            description
+            product_composition
+            calories
+            type
+          }
+        }
       }
     }
   }
