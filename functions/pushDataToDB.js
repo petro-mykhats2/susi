1582a84ea92eeec92819fdd53fab2befa69736f9
petro-mykhats2 <02webproject02@gmail.com>
2024-01-21 13:53:18 +0100

    The order data is now successfully sent to the Mongo database.

---
+++ functions/pushDataToDB.js
@@ -0,0 +1,56 @@
+const { MongoClient } = require('mongodb')
+
+const uri = process.env.URI_DATABASE_SUSI
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const connectToDatabase = async () => {
+  await client.connect()
+  return client.db('Susi').collection('Zamovlennia')
+}
+
+exports.handler = async (event) => {
+  try {
+    const collection = await connectToDatabase()
+
+    if (event.httpMethod === 'GET') {
+      // Handle GET request (fetch data)
+      const results = await collection.find({}).limit(20).toArray()
+      return {
+        statusCode: 200,
+        body: JSON.stringify(results),
+      }
+    } else if (event.httpMethod === 'POST') {
+      // Handle POST request (add data)
+      const data = JSON.parse(event.body)
+      const result = await collection.insertOne(data)
+      console.log('дані що надсилаються ', data)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Data added successfully',
+          insertedId: result.insertedId,
+        }),
+      }
+    } else {
+      // Handle other types of requests
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Invalid request method' }),
+      }
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Server error',
+        error: error.message,
+      }),
+    }
+  } finally {
+    await client.close()
+  }
+}
