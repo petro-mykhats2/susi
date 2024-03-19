9f95caf86a1b02555bb2e60dc0c29864cd000dfe
petro-mykhats2 <02webproject02@gmail.com>
2024-03-19 18:23:28 +0100

    telegram bot is ready

--- functions/telegram.js
+++ functions/telegram.js
@@ -1,43 +1,66 @@
-// const TelegramBot = require('node-telegram-bot-api')
-// // const token = process.env.YOUR_TELEGRAM_BOT_TOKEN
-// // const token = process.env.SUSHI_TELEGRAM_TOKEN
-// const token = '6462249989:AAF3uURDfNwTQp3RDT_X-4vzWWiL36pOkUg'
-// const bot = new TelegramBot(token, { polling: false })
+const TelegramBot = require('node-telegram-bot-api')
+const token = process.env.SUSHI_TELEGRAM_TOKEN
+const bot = new TelegramBot(token, { polling: false })
 
-// exports.handler = async function (event, context) {
-//   if (event.httpMethod !== 'POST') {
-//     return { statusCode: 405, body: 'Method Not Allowed' }
-//   }
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
 
-//   if (!event.body) {
-//     return { statusCode: 400, body: 'Body is empty!' }
-//   }
+  if (!event.body) {
+    return { statusCode: 400, body: 'Body is empty!' }
+  }
 
-//   const chatId = 735449634
-//   const requestBody = JSON.parse(event.body)
-//   const { name, email, message } = requestBody
+  const chatId = 735449634
+  const requestBody = JSON.parse(event.body)
+  const {
+    name,
+    phone,
+    message,
+    deliveryFormData,
+    timeFormData,
+    accessoriesData,
+    cartData,
+  } = requestBody
 
-//   // const formattedCartItems = cartItems
-//   //   .map((item) => {
-//   //     return `${item.name} - К-сть: ${item.quantity} - Ціна: $${
-//   //       item.price
-//   //     } - Сумма: $${item.price * item.quantity}`
-//   //   })
-//   //   .join('\n')
+  // Отримання даних про замовника
+  const customerInfo = `Ім'я: ${name}\nТелефон: ${phone}\nПовідомлення: ${message}`
 
-//   try {
-//     await bot.sendMessage(
-//       chatId,
-//       `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nZakaz:`
-//     )
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ message: 'Message sent' }),
-//     }
-//   } catch (error) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ message: 'Error sending message' }),
-//     }
-//   }
-// }
+  // Отримання даних про доставку
+  const deliveryInfo = `Опція доставки: ${deliveryFormData.deliveryOption}\nАдреса для самовивозу: ${deliveryFormData.pickupAddress}`
+
+  // Отримання даних про час доставки
+  const timeInfo = `Опція часу доставки: ${timeFormData.deliveryTimeOption}\nНайближча година: ${timeFormData.nextHour}`
+
+  // Отримання даних про аксесуари
+  const accessoriesInfo = `Кількість аксесуарів: ${
+    accessoriesData.quantity
+  }\nНавчальні палочки: ${accessoriesData.educational ? 'Так' : 'Ні'}`
+
+  // Отримання даних про замовлення
+  const cartItemsInfo = cartData
+    .map(
+      (item) =>
+        `${item.name} - Кількість: ${item.quantity} - Ціна: ${
+          item.price
+        } грн - Сума: ${item.price * item.quantity} грн`
+    )
+    .join('\n')
+
+  // Складання всіх даних в один текстовий рядок
+  const messageToSend = `${customerInfo}\n\nДані доставки:\n${deliveryInfo}\n\nДані про час доставки:\n${timeInfo}\n\nДані про аксесуари:\n${accessoriesInfo}\n\nЗамовлення:\n${cartItemsInfo}`
+
+  try {
+    // Відправка повідомлення
+    await bot.sendMessage(chatId, messageToSend)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Повідомлення відправлено' }),
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Помилка відправки повідомлення' }),
+    }
+  }
+}
--- src/components/CartSlider.js
+++ src/components/CartSlider.js
@@ -70,12 +70,24 @@ const CartSlider = ({ isOpen, onClose }) => {
         }),
       }
 
+      // Send data to MongoDB
       const response = await fetch(
         '/.netlify/functions/pushDataToDB',
         requestOptions
       )
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+
+      // Send data to Telegram
+      const telegramResponse = await fetch(
+        '/.netlify/functions/telegram',
+        requestOptions
+      )
+      if (!telegramResponse.ok) {
+        throw new Error(
+          `Error sending message to Telegram bot! Status: ${telegramResponse.status}`
+        )
       }
 
       setSuccessMessage('Ваше замовлення успішно виконано!')
@@ -96,15 +108,14 @@ const CartSlider = ({ isOpen, onClose }) => {
       dispatch(clearCart())
     } catch (error) {
       console.error('Error sending the message:', error)
-      setTimeout(() => {
-        setErrorMessage('')
-      }, 2000)
+      setErrorMessage('Помилка при відправленні замовлення')
     } finally {
       setTimeout(() => {
         setSuccessMessage('')
+        setErrorMessage('')
+        setIsOrdering(false)
       }, 4000)
     }
-    setIsOrdering(false)
   }
 
   const handleInputChange = (e) => {
@@ -155,9 +166,9 @@ const CartSlider = ({ isOpen, onClose }) => {
               placeholder='Ваше імя'
             />
           </div>
-          {nameError ? (
-            <p className='errorMessage'>заповніить правильно імя!</p>
-          ) : null}
+          {nameError && (
+            <p className='errorMessage'>Заповніть правильно ім'я!</p>
+          )}
           <div className={`form-field ${phoneError ? 'error' : ''}`}>
             <input
               type='phone'
@@ -168,13 +179,10 @@ const CartSlider = ({ isOpen, onClose }) => {
               placeholder='Ваш номер телефону'
             />
           </div>
-          {phoneError ? (
-            <p className='errorMessage'>заповніить правильно номер телефону!</p>
-          ) : null}
-          <DeliveryForm
-            handleDeliveryFormData={handleDeliveryFormData}
-            addressError={addressError}
-          />
+          {phoneError && (
+            <p className='errorMessage'>Заповніть правильно номер телефону!</p>
+          )}
+          <DeliveryForm handleDeliveryFormData={handleDeliveryFormData} />
           <DeliveryTime handleTimeFormData={handleTimeFormData} />
           <Accessories onChange={handleAccessoriesChange} />
           <div className='form-field'>
@@ -184,11 +192,11 @@ const CartSlider = ({ isOpen, onClose }) => {
               name='message'
               value={formData.message}
               onChange={handleInputChange}
-              placeholder='Напишіть ваші побажання або зауваження (необовязково)'
+              placeholder='Напишіть ваші побажання або зауваження (необов’язково)'
             />
           </div>
           <button
-            className='item-buttom_button  button_submit'
+            className='item-buttom_button button_submit'
             type='submit'
             disabled={isOrdering}
           >
@@ -203,3 +211,209 @@ const CartSlider = ({ isOpen, onClose }) => {
 }
 
 export default CartSlider
+
+// import React, { useEffect, useState } from 'react'
+// import Cart from './Cart'
+// import fetch from 'cross-fetch'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { clearCart } from '../redux/cart'
+// import DeliveryForm from './DeliveryForm'
+// import DeliveryTime from './DeliveryTime'
+// import Accessories from './Accessories'
+
+// const CartSlider = ({ isOpen, onClose }) => {
+//   const [isOrdering, setIsOrdering] = useState(false)
+//   const [successMessage, setSuccessMessage] = useState('')
+//   const [errorMessage, setErrorMessage] = useState('')
+//   const [nameError, setNameError] = useState(false)
+//   const [phoneError, setPhoneError] = useState(false)
+//   const [addressError, setAddressError] = useState(false)
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     phone: '',
+//     message: '',
+//     deliveryFormData: {},
+//     timeFormData: {},
+//     cartData: [],
+//     accessoriesData: {
+//       quantity: 2,
+//       educational: false,
+//     },
+//   })
+
+//   const dispatch = useDispatch()
+//   const cartItems = useSelector((state) => state.cart.cartItems)
+
+//   useEffect(() => {
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       cartData: cartItems,
+//     }))
+//   }, [cartItems])
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
+
+//     if (
+//       !formData.name ||
+//       !formData.phone ||
+//       !formData.deliveryFormData.pickupAddress
+//     ) {
+//       setNameError(!formData.name)
+//       setPhoneError(!formData.phone)
+//       setAddressError(true)
+//       return
+//     }
+
+//     setIsOrdering(true)
+
+//     try {
+//       const requestOptions = {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           name: formData.name,
+//           message: formData.message,
+//           phone: formData.phone,
+//           deliveryFormData: formData.deliveryFormData,
+//           timeFormData: formData.timeFormData,
+//           accessoriesData: formData.accessoriesData,
+//           cartData: formData.cartData,
+//         }),
+//       }
+
+//       const response = await fetch(
+//         '/.netlify/functions/pushDataToDB',
+//         requestOptions
+//       )
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`)
+//       }
+
+//       setSuccessMessage('Ваше замовлення успішно виконано!')
+//       setErrorMessage('')
+
+//       setFormData({
+//         name: '',
+//         phone: '',
+//         message: '',
+//         deliveryFormData: {},
+//         timeFormData: {},
+//         cartData: [],
+//         accessoriesData: {
+//           quantity: 2,
+//           educational: false,
+//         },
+//       })
+//       dispatch(clearCart())
+//     } catch (error) {
+//       console.error('Error sending the message:', error)
+//       setTimeout(() => {
+//         setErrorMessage('')
+//       }, 2000)
+//     } finally {
+//       setTimeout(() => {
+//         setSuccessMessage('')
+//       }, 4000)
+//     }
+//     setIsOrdering(false)
+//   }
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target
+//     setFormData({ ...formData, [name]: value })
+//     if (name === 'name') setNameError(!value)
+//     if (name === 'phone') setPhoneError(!value)
+//   }
+
+//   const handleDeliveryFormData = (data) => {
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       deliveryFormData: data,
+//     }))
+//   }
+
+//   const handleTimeFormData = (data) => {
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       timeFormData: data,
+//     }))
+//   }
+
+//   const handleAccessoriesChange = (accessoriesData) => {
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       accessoriesData: accessoriesData,
+//     }))
+//   }
+
+//   return (
+//     <div className={`cart-slider ${isOpen ? 'open' : ''}`}>
+//       <div className='cart-header'>
+//         <h2>Корзина</h2>
+//         <button onClick={onClose}>Закрити</button>
+//       </div>
+//       <Cart />
+//       {cartItems && cartItems.length === 0 ? null : (
+//         <form onSubmit={handleSubmit} className='form-checkoutBlock'>
+//           <h3 className='form-field'>Оформити замовлення</h3>
+//           <div className={`form-field ${nameError ? 'error' : ''}`}>
+//             <input
+//               type='text'
+//               id='name'
+//               name='name'
+//               value={formData.name}
+//               onChange={handleInputChange}
+//               placeholder='Ваше імя'
+//             />
+//           </div>
+//           {nameError ? (
+//             <p className='errorMessage'>заповніить правильно імя!</p>
+//           ) : null}
+//           <div className={`form-field ${phoneError ? 'error' : ''}`}>
+//             <input
+//               type='phone'
+//               id='phone'
+//               name='phone'
+//               value={formData.phone}
+//               onChange={handleInputChange}
+//               placeholder='Ваш номер телефону'
+//             />
+//           </div>
+//           {phoneError ? (
+//             <p className='errorMessage'>заповніить правильно номер телефону!</p>
+//           ) : null}
+//           <DeliveryForm
+//             handleDeliveryFormData={handleDeliveryFormData}
+//             addressError={addressError}
+//           />
+//           <DeliveryTime handleTimeFormData={handleTimeFormData} />
+//           <Accessories onChange={handleAccessoriesChange} />
+//           <div className='form-field'>
+//             <textarea
+//               type='text'
+//               id='message'
+//               name='message'
+//               value={formData.message}
+//               onChange={handleInputChange}
+//               placeholder='Напишіть ваші побажання або зауваження (необовязково)'
+//             />
+//           </div>
+//           <button
+//             className='item-buttom_button  button_submit'
+//             type='submit'
+//             disabled={isOrdering}
+//           >
+//             Замовити
+//           </button>
+//         </form>
+//       )}
+//       {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+//       {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+//     </div>
+//   )
+// }
+
+// export default CartSlider
